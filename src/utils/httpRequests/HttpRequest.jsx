568eca98c6c25695db26824099b8b275de2b4941
stvnlee890 <stvnlee890@gmail.com>
2022-10-11 23:17:14 -0700

    still working on this component, just a test for now. Will create a dev branch for this

--- src/utils/httpRequests/HttpRequest.jsx
+++ src/utils/httpRequests/HttpRequest.jsx
@@ -5,11 +5,21 @@ import { useNavigate } from "react-router-dom";
 const baseUrl = "http://localhost:8800/api";
 
 export const PostSignUp = async (body) => {
-    const navigate = useNavigate
+  const navigate = useNavigate;
   try {
     const res = await axios.post(`${baseUrl}/users/signup`, body);
     const data = await res.data;
-    return data
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const PostLogin = async (body) => {
+  try {
+    const res = await axios.post(`${baseUrl}/users/login`, body);
+    const data = await res.data;
+    return data;
   } catch (error) {
     console.log(error);
   }
