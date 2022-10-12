52b9953d9d8adc13d498ef3f58a7c61713c8ed07
stvnlee890 <stvnlee890@gmail.com>
2022-10-11 23:18:10 -0700

    added Logout component, conditionally render components based on user log in state

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,54 +1,85 @@
 import React from "react";
 import "./topbar.css";
 import { Link } from "react-router-dom";
+import Logout from "../userAuth/logout/Logout";
 
-const Topbar = () => {
-  return (
-    <>
-      <div className="topbarContainer">
-        <div className="topbarLeft">
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <span className="logo">Home</span>
-          </Link>
+const Topbar = ({ isLoggedIn, setIsLoggedIn }) => {
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+  }
+  if (isLoggedIn) {
+    return (
+      <>
+        <div className="topbarContainer">
+          <div className="topbarLeft">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <span className="logo">Home</span>
+            </Link>
+          </div>
+          <div className="topbarCenter">
+            <div className="post">
+              <Link to="/posts" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Posts</span>
+              </Link>
+            </div>
+            <div className="developers">
+              <Link to="/developers" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Developers</span>
+              </Link>
+            </div>
+
+            <div className="login developers">
+              <Link onClick={handleLogout} to="/logout" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Logout</span>
+              </Link>
+            </div>
+          </div>
+
+          <div className="topbarLeft"></div>
         </div>
-        <div className="topbarCenter">
-          <div className="post">
-            <Link to="/posts" style={{ textDecoration: "none" }}>
-              <span className="topbarLink">Posts</span>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="topbarContainer">
+          <div className="topbarLeft">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <span className="logo">Home</span>
             </Link>
           </div>
-          <div className="developers">
-            <Link to="/developers" style={{ textDecoration: "none" }}>
-              <span className="topbarLink">Developers</span>
-            </Link>
+          <div className="topbarCenter">
+            <div className="post">
+              <Link to="/posts" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Posts</span>
+              </Link>
+            </div>
+            <div className="developers">
+              <Link to="/developers" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Developers</span>
+              </Link>
+            </div>
+            <div className="login developers">
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Login</span>
+              </Link>
+            </div>
+            <div className="login developers">
+              <Link to="/signup" style={{ textDecoration: "none" }}>
+                <span className="topbarLink">Sign Up</span>
+              </Link>
+            </div>
           </div>
-          <div className="login developers">
-            <Link to="/login" style={{ textDecoration: "none" }}>
+
+          <div className="topbarLeft">
+            {/* <Link to="/login" style={{ textDecoration: "none" }}>
               <span className="topbarLink">Login</span>
-            </Link>
-          </div>
-          
-          <div className="login developers">
-            <Link to="/logout" style={{ textDecoration: "none" }}>
-              <span className="topbarLink">Logout</span>
-            </Link>
-          </div>
-
-          <div className="login developers">
-            <Link to="/signup" style={{ textDecoration: "none" }}>
-              <span className="topbarLink">Sign Up</span>
-            </Link>
+            </Link> */}
           </div>
         </div>
+      </>
+    );
+  }
+};
 
-        <div className="topbarLeft">
-          {/* <Link to="/login" style={{ textDecoration: "none" }}>
-            <span className="topbarLink">Login</span>
-          </Link> */}
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default Topbar; 
+export default Topbar;
