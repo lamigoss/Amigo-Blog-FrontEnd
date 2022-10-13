25f6f68c4ca0e6c3717d6dcd53151d3f8a77033d
stvnlee890 <stvnlee890@gmail.com>
2022-10-12 23:26:23 -0700

    able to upload images from frontend to aws and db

--- src/components/AUTH_users/ImageUpload/ImageUpload.jsx
+++ src/components/AUTH_users/ImageUpload/ImageUpload.jsx
@@ -1,18 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const ImageUpload = () => {
-    const [uploadImages, setUploadImages] = useState()
+  const [uploadImages, setUploadImages] = useState();
 
-    const formData = new FormData()
-    formData.append('image', uploadImages)
-    return (
-        <div>
-            <form>
-                <input type='file'></input>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
-}
+
+  const formData = new FormData();
+  formData.append("image", uploadImages);
+
+  const handleChange = (event) => {
+    const files = event.target.files[0];
+    setUploadImages(files);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8800/api/images", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err))
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleChange}></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default ImageUpload;
