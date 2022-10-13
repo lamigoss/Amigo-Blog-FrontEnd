46767cf786b3fde19f68c98908a1b95f1b5dce67
RoYce1993 <swhong0419@gmail.com>
2022-10-13 11:20:11 -0700

    post component organized

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Topbar from "./components/topbar/Topbar";
 import Signup from "./components/userAuth/signup/Singup";
 import Logout from "./components/userAuth/logout/Logout";
 import BlogPostPage from "./pages/blogPostPage/BlogPostPage";
-import PostForm from "./components/posts/PostForm";
+import PostForm from "./components/posts/writePost/PostForm";
+import SinglePost from "./components/posts/singlePost/SinglePost";
 
 const App = () => {
   const context = useContext(AuthContext)
@@ -19,6 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<BlogPostPage />} />
+        <Route path="/posts/:postId" element={<SinglePost />} />
         <Route path="/posts/create" element={<PostForm />} />
         <Route path="/developers" element={<Developers />} />
         <Route path="/login" element={<Login setIsLoggedIn={context.setIsLoggedIn}/>} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Posts from "../../components/posts/Posts";
 
 export default function Home() {
   return <>this is the home page</>;
 }
-
-// checks
