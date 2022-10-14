4c7f6b7f9f1bce15f8aaa9c44d1ac6e6b10e6e02
RoYce1993 <swhong0419@gmail.com>
2022-10-14 00:02:52 -0700

    post/put/delete/get api calls functioning with minor exceptions

--- src/components/posts/singlePost/SinglePost.jsx
+++ src/components/posts/singlePost/SinglePost.jsx
@@ -7,37 +7,89 @@ export default function SinglePost() {
   const [post, setPost] = useState({});
   const location = useLocation();
   const navigate = useNavigate();
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+  const [updateMode, setUpdateMode] = useState(false);
 
   // const post = "http://localhost:3000/posts/63460e4115d974d2c5a118ac";
 
   const postId = location.pathname.split("/")[2];
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getPost = async () => {
       try {
         const res = await axios.get(`/posts/${postId}`);
         setPost(res.data);
-        console.log(res.data);
-        console.log(navigate);
+        setTitle(res.data.postTitle);
+        setDesc(res.data.postDesc);
       } catch (error) {}
     };
-    fetchData();
+    getPost();
   }, []);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {});
+      window.location.replace("/");
+    } catch (err) {}
+  };
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${postId}`, {
+        _id: postId,
+        userId: "6344c9d878ed99b6e34b2cdf", // this userId needs to be ref not hardcoded
+        postTitle: title,
+        postDesc: desc,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setUpdateMode(false);
+    console.log("update clicked");
+  };
 
   return (
     <div className="singlePostContainer">
+      <button
+        className="singlePostIcon far fa-edit"
+        onClick={() => setUpdateMode(true)}
+      >
+        update this post
+      </button>
       <div className="singlePostWrapper">
-        <img src={post.postImg} alt="" className="singlePostImg" />
-        <h1 className="singlePostTitle">{post.postTitle}</h1>
+        {post.postImg && <img src={""} alt="" className="singlePostImg" />}
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="singlePostTitleInput"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="singlePostTitle">{title}</h1>
+        )}
+        {post.postTitle && (
+          <h1 className="singlePostTitle">{post.postTitle}</h1>
+        )}
         <div className="singlePostDesc">{post.postDesc}</div>
-      </div>
-      <div className="button">
-        <Link to="/posts">
-          <button className="goBacktoPost">Go Back to Posts Page</button>
-        </Link>
-        <Link to="/posts/create">
-          <button className="goBacktoPost">Update this post</button>
-        </Link>
+        {updateMode ? (
+          <textarea
+            className="singlePostDescinput"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="singlePostDesc">{desc}</p>
+        )}
+
+        {updateMode && (
+          <button className="singlePostButton" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
+        <button onClick={handleDelete}>Delete this post</button>
       </div>
     </div>
   );
