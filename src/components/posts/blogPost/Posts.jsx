ec96e716c80165f9301b7e8b5321768f63d76b1e
RoYce1993 <swhong0419@gmail.com>
2022-10-14 19:17:10 -0700

    click to.. post

--- src/components/posts/blogPost/Posts.jsx
+++ src/components/posts/blogPost/Posts.jsx
@@ -5,27 +5,28 @@ const Posts = ({ post }) => {
   return (
     <>
       <div className="postsContainer">
-        <Link to={`/posts/${post._id}`}>
-          <div className="postsWrapper">
-            <div className="postsTop">
-              <div className="postsTopLeft">
-                <img
-                  src="/assets/person/1.jpeg"
-                  alt=""
-                  className="profilePicture"
-                />
-                <span className="userName">{post.username}</span>
-                {/* <span className="postDate">{format(post.createdAt)}</span> */}
-              </div>
-              <div className="postsCeter">
-                <span className="postTitle">{post.postTitle}</span>
-                <img src="/assets/posts/1.jpeg" alt="" className="postsImg" />
-              </div>
-              <div className="postsBottom">
-                <span className="postsDesc">{post.postDesc}</span>
-              </div>
+        <div className="postsWrapper">
+          <div className="postsTop">
+            <div className="postsTopLeft">
+              <img
+                src="/assets/person/1.jpeg"
+                alt=""
+                className="profilePicture"
+              />
+              <span className="userName">{post.username}</span>
+              {/* <span className="postDate">{format(post.createdAt)}</span> */}
+            </div>
+            <div className="postsCeter">
+              <span className="postTitle">{post.postTitle}</span>
+              <img src="/assets/posts/1.jpeg" alt="" className="postsImg" />
+            </div>
+            <div className="postsBottom">
+              <span className="postsDesc">{post.postDesc}</span>
             </div>
           </div>
+        </div>
+        <Link to={`/posts/${post._id}`}>
+          <button className="btn btn-primary">Click to view the post</button>
         </Link>
       </div>
     </>
