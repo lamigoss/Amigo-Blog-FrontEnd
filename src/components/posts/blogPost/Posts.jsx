234064f6ca1eb4893169a08bc854958416a60aa7
RoYce1993 <swhong0419@gmail.com>
2022-10-14 19:02:05 -0700

    post component updated

--- src/components/posts/blogPost/Posts.jsx
+++ src/components/posts/blogPost/Posts.jsx
@@ -1,55 +1,33 @@
-import React, { useState, useEffect } from "react";
 import "./posts.css";
-// import { format } from "timeago.js";
-import axios from "axios";
-import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
-import Topbar from "../../topbar/Topbar";
-
+import { Link } from "react-router-dom";
 
 const Posts = ({ post }) => {
-  // const [posts, setPosts] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await axios.get(`/`);
-  //     setPosts(res.data);
-  //   };
-  //   fetchData();
-  // }, [posts.data]);
-
   return (
     <>
       <div className="postsContainer">
-        <div className="postsWrapper">
-          <div className="postsTop">
-            <div className="postsTopLeft">
-              <img
-                src="/assets/person/1.jpeg"
-                alt=""
-                className="profilePicture"
-              />
-              <span className="userName">{}</span>
-              {/* <span className="postDate">{format(post.createdAt)}</span> */}
-            </div>
-            <div className="postsCeter">
-              <span className="postTitle">{post.postTitle}</span>
-              <img src="/assets/posts/1.jpeg" alt="" className="postsImg" />
-            </div>
-            <div className="postsBottom">
-              <span className="postsDesc">{post.postDesc}</span>
-              <div className="postsBottomLeft">
-                {/* <img src="/assets/like.png" alt="" className="likeIcon" />
-                <ChatOutlinedIcon className="postsCommentText" /> */}
+        <Link to={`/posts/${post._id}`}>
+          <div className="postsWrapper">
+            <div className="postsTop">
+              <div className="postsTopLeft">
+                <img
+                  src="/assets/person/1.jpeg"
+                  alt=""
+                  className="profilePicture"
+                />
+                <span className="userName">{post.username}</span>
+                {/* <span className="postDate">{format(post.createdAt)}</span> */}
               </div>
-
-              {/* <div className="postsBottomRight">
-                <ChatOutlinedIcon className="postsCommentText" />
-              </div> */}
+              <div className="postsCeter">
+                <span className="postTitle">{post.postTitle}</span>
+                <img src="/assets/posts/1.jpeg" alt="" className="postsImg" />
+              </div>
+              <div className="postsBottom">
+                <span className="postsDesc">{post.postDesc}</span>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
-      {/* <ImageUpload /> */}
     </>
   );
 };
