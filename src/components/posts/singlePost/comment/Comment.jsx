f42761e5b0ae9151d36540eea76233a64eac91f4
stvnlee890 <stvnlee890@gmail.com>
2022-10-21 23:18:06 -0700

    rebasing comment component

--- src/components/posts/singlePost/comment/Comment.jsx
+++ src/components/posts/singlePost/comment/Comment.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GetComment } from "../../../../utils/httpRequests/HttpRequest";
 
-const Comment = ({ comment }) => {
-  //   const username = window.localStorage.getItem("user");
-  console.log("this is the ...:" + comment);
+import "./comment.css";
+const Comment = () => {
+  const [writeMode, setWriteMode] = useState(false);
+  const [comments, setComments] = useState();
 
+  const { postId } = useParams();
+
+  useEffect(() => {
+    GetComment(postId)
+      .then((res) => setComments(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(comments);
   return (
-    <div className="container">
-      <div className="details">
-        <div className="name">{comment.username}</div>
-        <div className="text"> {comment.desc}</div>
+    <>
+      <div className="commentContainer">
+        <div className="user">User Name</div>
+        <div className="comment">comments</div>
+        {comments && comments.map((ele) => <p key={ele.id}>{ele.desc}</p>)}
+        {!comments && <p>no comments</p>}
+        <div className="timeStamp">timeStamp</div>
       </div>
-    </div>
+    </>
   );
 };
+
+// #endregion
 
 export default Comment;
