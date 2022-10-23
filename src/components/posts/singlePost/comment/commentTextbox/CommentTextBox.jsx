86219f5475f843ee27f824b8e5b5368e3a8883c4
RoYce1993 <swhong0419@gmail.com>
2022-10-22 18:16:57 -0700

    comment conflict resolved

--- src/components/posts/singlePost/comment/commentTextbox/CommentTextBox.jsx
+++ src/components/posts/singlePost/comment/commentTextbox/CommentTextBox.jsx
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./commentTextBox.css";
-
+import { useLocation } from "react-router-dom";
 const CommentTextBox = () => {
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState({
+    username: "",
+    postId: "",
+    desc: "",
+  });
+
+  const user = window.localStorage.getItem("user");
+  const location = useLocation();
+
+  const postId = location.pathname.split("/")[2];
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setComment({ postId: postId, desc: text, username: user });
+  };
+
+  const handleComment = () => {
+    const res = axios.post("/comments", comment);
+    console.log("comment working?: " + comment);
+    console.log("res.data: " + res.data);
+    return res.data;
+  };
   return (
     <>
-      <input
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        type="text"
-        className="commentTextbox"
-        placeholder="write your thoughts about this blog post and share with other readers!"
-      />
-      <button>Submit</button>
+      <form className="container" onSubmit={handleComment}>
+        <input
+          type="text"
+          className="commentTextbox"
+          placeholder="write your thoughts about this blog post and share with other readers!"
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
-
-export default CommentTextBox;
