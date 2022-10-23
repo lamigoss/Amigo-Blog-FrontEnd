be88f02d16396d621139814d12355173f38fa4fd
stvnlee890 <stvnlee890@gmail.com>
2022-10-23 15:58:36 -0700

    trying to pull main

--- src/components/posts/singlePost/comment/Comment.jsx
+++ src/components/posts/singlePost/comment/Comment.jsx
@@ -1,34 +1,34 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { GetComment } from "../../../../utils/httpRequests/HttpRequest";
+// import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { GetComment } from "../../../../utils/httpRequests/HttpRequest";
 
-import "./comment.css";
-const Comment = () => {
-  const [writeMode, setWriteMode] = useState(false);
-  const [comments, setComments] = useState();
+// import "./comment.css";
+// const Comment = () => {
+//   const [writeMode, setWriteMode] = useState(false);
+//   const [comments, setComments] = useState();
 
-  const { postId } = useParams();
+//   const { postId } = useParams();
 
-  useEffect(() => {
-    GetComment(postId)
-      .then((res) => setComments(res.data))
-      .catch((err) => console.log(err));
-  }, []);
-  console.log(comments);
-  return (
-    <>
-      <div className="commentContainer">
-        <div className="user">User Name</div>
-        <div className="comment">comments</div>
-        {comments && comments.map((ele) => <p key={ele.id}>{ele.desc}</p>)}
-        {!comments && <p>no comments</p>}
-        <div className="timeStamp">timeStamp</div>
-      </div>
-    </>
-  );
-};
+//   useEffect(() => {
+//     GetComment(postId)
+//       .then((res) => setComments(res.data))
+//       .catch((err) => console.log(err));
+//   }, []);
+//   console.log(comments);
+//   return (
+//     <>
+//       <div className="commentContainer">
+//         <div className="user">User Name</div>
+//         <div className="comment">comments</div>
+//         {comments && comments.map((ele) => <p key={ele.id}>{ele.desc}</p>)}
+//         {!comments && <p>no comments</p>}
+//         <div className="timeStamp">timeStamp</div>
+//       </div>
+//     </>
+//   );
+// };
 
-// #endregion
+// // #endregion
 
-export default Comment;
+// export default Comment;
