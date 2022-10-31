88263f46555e337fcdba80196f063c8efcdc8fa5
RoYce1993 <swhong0419@gmail.com>
2022-10-31 16:54:35 -0700

    white and purple

--- src/components/dev/RoyceHong.jsx
+++ src/components/dev/RoyceHong.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Topbar from "../topbar/Topbar";
 import Leftbar from "../leftbar/Leftbar";
 
 export default function RoyceHong() {
   return (
     <>
-      <Topbar />
-      <div>Hello this is Royce Hong and I am really gay</div>
+      <div className="text-center">
+        <p>Hello this is Royce Hong and I am really gay</p>
+      </div>
       <Leftbar />
     </>
   );
