b32225c8a9f72841c2a5c82a864f6b0649f14ac8
RoYce1993 <swhong0419@gmail.com>
2022-11-01 16:07:08 -0700

    dev page

--- src/components/dev/SteveLee.jsx
+++ src/components/dev/SteveLee.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Leftbar from "../leftbar/Leftbar";
+import Steve from "../../img/steve.jpg";
 
 export default function SteveLee() {
   return (
     <>
       <div className="text-center">
+        <img src={Steve} alt="/" />
         <p> Hello this is Steve Lee and I am really gay</p>
       </div>
       <Leftbar />
