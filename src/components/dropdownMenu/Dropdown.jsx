b219e5f4eb37c46b4691f8cf1389d5f32e60cd5f
stvnlee890 <stvnlee890@gmail.com>
2022-11-04 13:04:27 -0700

    close dropdown menu when clicked outside the dropdown component

--- src/components/dropdownMenu/Dropdown.jsx
+++ src/components/dropdownMenu/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
@@ -6,10 +6,20 @@ import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 const Dropdown = () => {
   const [drop, setDrop] = useState(false);
   const navigate = useNavigate()
-
+ 
   const toggleDrop = () => {
     setDrop((prev) => !prev);
   };
+
+  useEffect(() => {
+    const closeDropDown = event => {
+      if(event.path[0].tagName !== 'svg')
+      setDrop(false)
+    }
+    document.body.addEventListener('click', closeDropDown)
+
+    return () => document.body.removeEventListener('click', closeDropDown)
+  },[])
   
   const handleNavigate = (event) => {
    if (event.target.id === 'stevelee'){
@@ -21,28 +31,31 @@ const Dropdown = () => {
    }
   }
 
+
   return (
     <>
       <div className="flex">
         <Link to="/developers">Developers</Link>
+        <div>
         {!drop ? (
-          <div className="h-2 w-2" onClick={toggleDrop}>
+          <button className="h-2 w-2" onClick={toggleDrop}>
             <ArrowDropDownIcon />
-          </div>
+          </button>
         ) : (
-          <div className="h-2 w-2" onClick={toggleDrop}>
+          <button className="h-2 w-2" onClick={toggleDrop}>
             <ArrowDropUpIcon />
-          </div>
+          </button>
         )}
+        </div>
       </div>
 
       {drop ? (
-        <div className="h-40 w-40 bg-white text-purple rounded-md shadow-md absolute">
+        <div className="grid h-32 w-32 bg-white text-purple rounded-md shadow-md absolute">
           <Link to={"/developers/stevelee"}>
-            <div onClick={handleNavigate} id='stevelee'className="hover:bg-indigo-300 p-5">Steve Lee</div>
+            <div onClick={handleNavigate} id='stevelee' className=" text-center hover:bg-indigo-300 p-5">Steve Lee</div>
           </Link>
           <Link to={"/developers/roycehong"}>
-            <div onClick={handleNavigate} id='roycehong' className="hover:bg-indigo-300 p-5">Roy Hong</div>
+            <div onClick={handleNavigate} id='roycehong' className="text-center hover:bg-indigo-300 p-5">Roy Hong</div>
           </Link>
         </div>
       ) : null}
