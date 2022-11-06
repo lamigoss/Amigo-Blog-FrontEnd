9b2d7161359bddd90c2b7ecde2fbbdc5b795757c
RoYce1993 <swhong0419@gmail.com>
2022-11-05 19:00:32 -0700

    none

--- src/components/dev/Roy/DevRoy.jsx
+++ src/components/dev/Roy/DevRoy.jsx
@@ -5,47 +5,83 @@ import { Link } from "react-router-dom";
 const DevRoy = () => {
   return (
     <>
-      <div className="bg-blue-600 ">
-        <div className="text-white h-100 w-100 text-8xl text-bold absolute ">
-          <div className="flex mx-80">
-            <p className="text-yellow-500">R</p>
-            <p>oyal</p>
+      <div className="bg-black">
+        <img
+          className="w-1/12"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          alt="/"
+        />
+
+        {/* <div className="h-screen w-100 tablet:text-8xl mobile:text-6xl md:text-bold">
+          <div className="flex md:mx-80 object-contain relative mobile:mt-32 mobile:mx-20">
+            <p className="text-yellow-500 animate-pulse">R</p>
+            <p className="text-red-500 animate-pulse">o</p>
+            <p className="text-blue-500 animate-ping">y</p>
+            <p className="text-green-500 opacity-0 hover:opacity-100">a</p>
+            <p className="text-blue-300 opacity-0 hover:opacity-100">l</p>
           </div>
-          <div className="flex mx-60">
-            <p>h</p> <p className="text-yellow-500">O</p> <p>nest</p>
+          <div className="flex md:mx-60 mobile:mx-16">
+            <p className="text-purple-600 opacity-0 hover:opacity-100">h</p>
+            <p className="text-yellow-500 hover:scale-150">O</p>
+            <p className="text-green-400 opacity-0 hover:opacity-100">n</p>
+            <p className="text-red-300 opacity-0 hover:opacity-100">e</p>
+            <p className="text-teal-400 opacity-0 hover:opacity-100">s</p>
+            <p className="text-cyan-600 opacity-0 hover:opacity-100">t</p>
           </div>
 
-          <div className="flex mx-8">
-            <p>prett</p>
-            <p className="text-yellow-500">Y</p>
+          <div className="flex md:mx-20 mb-10">
+            <p className=" text-red-700 opacity-0 hover:opacity-100">p</p>
+            <p className="text-lime-300 opacity-0 hover:opacity-100">r</p>
+            <p className="text-emerald-700 opacity-0 hover:opacity-100">e</p>
+            <p className="text-violet-400 opacity-0 hover:opacity-100">t</p>
+            <p className="text-rose-600 opacity-0 hover:opacity-100">t</p>
+            <p className="text-yellow-500 hover:scale-150">Y</p>
           </div>
-          <div className="flex mx-96">
-            <p>s</p>
-            <p className="text-yellow-500">H</p>
-            <p>y</p>
+          <div className="flex md:mx-60">
+            <p className=" text-pink-400 opacity-0 hover:opacity-100">s</p>
+            <p className="text-yellow-500 hover:scale-150">H</p>
+            <p className="text-blue-600 opacity-0 hover:opacity-100">y</p>
           </div>
-          <div className="flex mx-20">
-            <p>hardw</p>
-            <p className="text-yellow-500">O</p>
-            <p>rking</p>
+          <div className="flex md:mx-5">
+            <p className=" text-green-600 opacity-0 hover:opacity-100">h</p>
+            <p className=" text-pink-200 opacity-0 hover:opacity-100">a</p>
+            <p className=" text-pink-400 opacity-0 hover:opacity-100">r</p>
+            <p className=" text-blue-600 opacity-0 hover:opacity-100">d</p>
+            <p className=" text-red-600 opacity-0 hover:opacity-100">w</p>
+            <p className="text-yellow-500 hover:scale-150">O</p>
+            <p className=" text-sky-600 opacity-0 hover:opacity-100">r</p>
+            <p className=" text-orange-400 opacity-0 hover:opacity-100">k</p>
+            <p className=" text-emerald-500 opacity-0 hover:opacity-100">i</p>
+            <p className=" text-pink-400 opacity-0 hover:opacity-100">n</p>
+            <p className=" text-teal-600 opacity-0 hover:opacity-100">g</p>
           </div>
-          <div className="flex mx-24">
-            <p>i</p>
-            <p className="text-yellow-500">N</p>
-            <p>dividual</p>
+          <div className="flex md:mx-60">
+            <p className=" text-pink-200 opacity-0 hover:opacity-100">i</p>
+            <p className="text-yellow-500 hover:scale-150">N</p>
+            <p className=" text-green-400 opacity-0 hover:opacity-100">d</p>
+            <p className=" text-zinc-400 opacity-0 hover:opacity-100">i</p>
+            <p className=" text-red-500 opacity-0 hover:opacity-100">v</p>
+            <p className=" text-lime-400 opacity-0 hover:opacity-100">i</p>
+            <p className=" text-indigo-400 opacity-0 hover:opacity-100">d</p>
+            <p className=" text-orange-500 opacity-0 hover:opacity-100">u</p>
+            <p className=" text-cyan-600 opacity-0 hover:opacity-100">a</p>
+            <p className=" text-pink-600 opacity-0 hover:opacity-100">l</p>
           </div>
-          <div className="flex">
-            <p className="text-yellow-500">G</p>
-            <p>amer</p>
+          <div className="flex md:mx-96 mobile:mx-60">
+            <p className="text-yellow-500 hover:scale-150">G</p>
+            <p className=" text-red-800 opacity-0 hover:opacity-100">a</p>
+            <p className=" text-violet-400 opacity-0 hover:opacity-100">m</p>
+            <p className=" text-green-500 opacity-0 hover:opacity-100">e</p>
+            <p className=" text-stone-500 opacity-0 hover:opacity-100">r</p>
           </div>
-        </div>
+        </div> */}
 
-        <div className="rounded-full scale-50">
+        {/* <div className="rounded-full scale-50">
           <Link to={"/developers/roycehong"}>
             <img className="rounded-full scale-50" src={Roy} alt="/" />
             <p className=" text-center">Roy Hong</p>
           </Link>
-        </div>
+        </div> */}
       </div>
     </>
   );
