58cf00591a671eb4ac1560ed52988cdd7bb466b4
stvnlee890 <stvnlee890@gmail.com>
2022-11-07 15:06:40 -0800

    removed white bg color

--- src/components/dev/Developers.jsx
+++ src/components/dev/Developers.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Developers = () => {
   return (
     <>
-      <div className="text-center bg-white">
+      <div className="text-center">
         <h1 className="underline md:text-2xl underline-offset-4 md:mx-40 mx-20 md:px-20 px-10 md:py-10 py-5">
           Introducing two amigos who created this blog
         </h1>
