0178b274b0c39c0aed2ee872179cf7e18474a853
RoYce1993 <swhong0419@gmail.com>
2022-11-07 13:54:19 -0800

    bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,13 +4,12 @@ import AuthContext from "../../authContext/authContext";
 import Dropdown from "../dropdownMenu/Dropdown";
 
 const Topbar = () => {
-  const [open, setOpen] = useState(false);
   const context = useContext(AuthContext);
 
   const handleLogout = () => {
     context.setIsLoggedIn(false);
   };
-
+  // mwxs:hidden
   return (
     <>
       <div className="mwxs:hidden grid laptop:grid-cols-2 p-6 text-sm	text-blue-800">
@@ -63,96 +62,6 @@ const Topbar = () => {
             <div className="col-end-5">
               <Link to="/posts/create">
                 <span className="xs:px-6 sm:px-8 p-2 text-white transition duration-100 rounded-md bg-indigo-500 hover:bg-indigo-700 justify-self-center mt-6 shadow-lg">
-                  Blog
-                </span>
-              </Link>
-            </div>
-          ) : null}
-        </div>
-      </div>
-
-   
-      <button
-        onClick={() => setOpen(true)}
-        className="xs:hidden cursor-pointer p-4 absolute right-2 top-0 space-y-1.5"
-      >
-        <div className="w-8 h-0.5 bg-gray-600"></div>
-        <div className="w-8 h-0.5 bg-gray-600"></div>
-        <div className="w-8 h-0.5 bg-gray-600"></div>
-      </button>
-      <div
-        id="mobile-menu"
-        className={`xs:hidden text-xs relative top-1 space-y-2 bg-white pt-2 w-full text-5xl flex flex-col justify-content-center ${
-          open ? "flex" : "hidden"
-        }`}
-      >
-        <button
-          onClick={() => setOpen(false)}
-          className="text-5xl mobile:text-3xl self-end px-6"
-        >
-          &times;
-        </button>
-        <div
-          className="flex flex-col min-h-screen items-center py-8"
-          aria-label="mobile"
-        >
-          <div className="w-full text-center py-6 hover:text-blue-400">
-            <Link onClick={() => setOpen(false)} to="/">
-              <span className="duration-100 hover:text-blue-400 text-lg">
-                Home
-              </span>
-            </Link>
-          </div>
-          <div className="w-full text-center py-6 hover:text-blue-400">
-            <Link onClick={() => setOpen(false)} to="/posts">
-              <span className="duration-100 hover:text-blue-400 text-lg">
-                Posts
-              </span>
-            </Link>
-          </div>
-          <div className="w-full text-center py-6 hover:text-blue-400">
-            <Link onClick={() => setOpen(false)} to="/developers">
-              <span className="duration-100 hover:text-blue-400 text-lg">
-                Developers
-              </span>
-            </Link>
-          </div>
-          {context.isLoggedIn ? (
-            <div className="w-full text-center py-6 hover:text-blue-400 text-lg">
-              <Link
-                onClick={() => {
-                  setOpen(false);
-                  handleLogout();
-                }}
-                to="/logout"
-              >
-                <span className="duration-100 hover:text-blue-400 text-lg">
-                  Logout
-                </span>
-              </Link>
-            </div>
-          ) : (
-            <>
-              <div className="w-full text-center py-6 hover:text-blue-400">
-                <Link onClick={() => setOpen(false)} to="/login">
-                  <span className="duration-100 hover:text-blue-400 text-lg">
-                    Login
-                  </span>
-                </Link>
-              </div>
-              <div className="w-full text-center py-6 hover:text-blue-400">
-                <Link onClick={() => setOpen(false)} to="/signup">
-                  <span className="duration-100 hover:text-blue-400 text-lg">
-                    Sign Up
-                  </span>
-                </Link>
-              </div>
-            </>
-          )}
-          {context.admin ? (
-            <div className="w-full text-center py-6 hover:text-blue-400">
-              <Link onClick={() => setOpen(false)} to="/posts/create">
-                <span className="duration-100 hover:text-blue-400 text-lg">
                   Blog
                 </span>
               </Link>
