7a4fd128df3ba4dd78bbfed0a6eb4f4e5a033df6
stvnlee890 <stvnlee890@gmail.com>
2022-11-07 14:19:35 -0800

    rebasing

--- src/components/dev/Steve/HeroSteve.jsx
+++ src/components/dev/Steve/HeroSteve.jsx
@@ -19,24 +19,8 @@ const HeroSteve = () => {
                 Hi I'm Seung Ki Lee!
               </h2>
             </div>
-<<<<<<< HEAD
-            <p className="text-gray-600 text-center md:my-10 md:mx-20 my-5 mx-0.5 md:text-lg text-sm ">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
-              et praesentium, cum omnis sit odit soluta obcaecati, placeat quas
-              illum nesciunt facilis repudiandae velit molestias non adipisci
-              eos laboriosam harum voluptatibus tempora quod pariatur.
-              Reiciendis illo consequuntur in iusto, enim tempore odit aliquam
-              non unde at possimus tenetur illum pariatur, magni, nobis qui
-              vero! Earum iste eos officiis suscipit distinctio labore unde
-              voluptatibus similique ratione, sequi accusantium consequuntur
-              delectus iusto! Autem quaerat sed laudantium quos error debitis
-              quia veniam at harum eaque! Totam ratione beatae est quas qui eos,
-              numquam unde delectus rerum, dolor dicta voluptatum et ex debitis
-              laboriosam?
-=======
             <p className="text-gray-600 text-center md:my-10 md:mx-20 my-5 mx-10">
-              Whenever I'm away from the screen, I enjoy getting myself into fierce ping pong rallies with my friends or other players.
->>>>>>> 26921ee (making small stying adjustments)
+              When I'm offline and away from the screen, I enjoy getting myself into fierce ping pong rallies with my friends or other players. I find
             </p>
           </div>
         </div>
