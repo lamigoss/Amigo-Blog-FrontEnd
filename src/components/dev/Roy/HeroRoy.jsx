d52f4368eca4d058579df6549c87191ec4b0aa0e
RoYce1993 <swhong0419@gmail.com>
2022-11-14 23:40:00 -0800

    letter

--- src/components/dev/Roy/HeroRoy.jsx
+++ src/components/dev/Roy/HeroRoy.jsx
@@ -13,19 +13,19 @@ const HeroRoy = () => {
               alt=""
             />
           </div>
-          <div className="text-center md:px-48 px-30">
+          <div className="md:px-48 px-30">
             <div className="border-b-4 border-neutral-400">
               <h2 className="text-4xl md:text-7xl font-bold text-blue-500 mb-3">
                 Hi! I'm Roy!!
               </h2>
             </div>
-            <p className="text-gray-600 text-center md:my-10 md:mx-30 my-5 mx-10 md:text-lg text-sm">
+            <p className="text-gray-600 md:my-10 md:mx-30 my-5 mx-10 md:text-lg text-sm first-letter:font-bold first-letter:text-4xl first-letter:float">
               Hello World! My name is Roy. I am a passionate software developer
               who constantly seeks opportunities to learn about new specialties
               in programming. My dream has always been to utilize my skills to
               improve people's lives, regardless of their status.
             </p>
-            <p className="text-gray-600 text-center md:my-10 md:mx-30 my-5 mx-10 md:text-lg text-sm">
+            <p className="text-gray-600  md:my-10 md:mx-30 my-5 mx-10 md:text-lg text-sm">
               The city of angels and Dodgers is my neighborhood but originally
               born and raised in Suwon, Republic of Korea. Whenever I am not
               sitting in front of the monitor, I either play with my cat or
@@ -34,7 +34,7 @@ const HeroRoy = () => {
               me, Don't hesitate to email me any time! We can grab cheeseburgers
               at In N Out; the best part is that
             </p>
-            <p className="text-gray-600 text-center md:my-10 md:mx-30 my-5 mx-10 md:text-2xl text-sm hover:animate-ping">
+            <p className="text-gray-600  md:my-10 md:mx-30 my-5 mx-10 md:text-2xl text-sm hover:animate-ping">
               <strong>THEY ARE ON ME!</strong>
             </p>
           </div>
